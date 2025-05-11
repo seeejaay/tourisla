15d44a441ce14190b456b21d6ea3ce9c8da14a65
Carl <carljustinem984@gmail.com>
2025-05-11 21:59:07 +0800

    sidebar don, except responsiveness

--- web/src/app/(Admin)/admin/dashboard/page.tsx
+++ web/src/app/(Admin)/admin/dashboard/page.tsx
@@ -1,8 +1,12 @@
 "use client";
 
+import Sidebar from "@/components/custom/sidebar";
+
 export default function DashboardPage() {
   return (
     <>
+      <Sidebar />
+      {/* Main content */}
       <div className="flex flex-col items-center justify-center min-h-screen py-2">
         <h1 className="text-4xl font-bold">Admin Dashboard</h1>
         <p className="mt-4 text-lg">Welcome to the admin dashboard!</p>
