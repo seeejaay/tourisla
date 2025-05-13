bcf6126b1bfde57f8e97cb10cce24123a0196678
hannahbeatrz <hannahbeatriz1995@gmail.com>
2025-05-13 20:32:22 +0800

    added category for announcements

--- backend/models/announceModel.js
+++ backend/models/announceModel.js
@@ -1,24 +1,24 @@
 const db = require("../db/index.js");
 
 const createAnnouncement = async (announcementData) => {
-  const { title, description, date_posted, location, image_url } =
+  const { title, description, date_posted, location, image_url, category } =
     announcementData;
 
   const result = await db.query(
-    "INSERT INTO announcements (title, description, date_posted, location, image_url) VALUES ($1, $2, COALESCE($3, NOW()), $4, $5) RETURNING *",
-    [title, description, date_posted, location, image_url]
+    "INSERT INTO announcements (title, description, date_posted, location, image_url, category) VALUES ($1, $2, COALESCE($3, NOW()), $4, $5, $6) RETURNING *",
+    [title, description, date_posted, location, image_url, category]
   );
 
   return result.rows[0];
 };
 
 const editAnnouncement = async (announcementId, announcementData) => {
-  const { title, description, date_posted, location, image_url } =
+  const { title, description, date_posted, location, image_url, category } =
     announcementData;
 
   const result = await db.query(
-    "UPDATE announcements SET title = $1, description = $2, date_posted = $3, location = $4, image_url = $5 WHERE id = $6 RETURNING *",
-    [title, description, date_posted, location, image_url, announcementId]
+    "UPDATE announcements SET title = $1, description = $2, date_posted = $3, location = $4, image_url = $5, category = $6 WHERE id = $7 RETURNING *",
+    [title, description, date_posted, location, image_url, category, announcementId]
   );
 
   return result.rows[0];
