340393dbb5372aa996d1013efae3fde79473561f
Kevin James <vinaskevinjames15@gmail.com>
2025-05-28 05:20:32 +0800

    upload image - tourist spot

--- backend/controllers/touristSpotController.js
+++ backend/controllers/touristSpotController.js
@@ -1,12 +1,14 @@
-const e = require("express");
-
 const {
   createTouristSpot,
   editTouristSpot,
   deleteTouristSpot,
   getAllTouristSpots,
   getTouristSpotById,
-} = require("../models/touristSpotModel.js");
+  uploadTouristSpotImages,
+  deleteTouristSpotImages,
+} = require("../models/touristSpotModel");
+
+const { s3Client, PutObjectCommand } = require("../utils/s3.js");
 
 const createTouristSpotController = async (req, res) => {
   try {
@@ -26,17 +28,17 @@ const createTouristSpotController = async (req, res) => {
       contact_number,
       email,
       facebook_page,
-      rules
+      rules,
     } = req.body;
 
-    const touristSpot = await createTouristSpot({
+    const spot = await createTouristSpot({
       name: name?.toUpperCase(),
       type: type?.toUpperCase(),
       description: description?.toUpperCase(),
       barangay: barangay?.toUpperCase(),
       municipality: municipality?.toUpperCase(),
       province: province?.toUpperCase(),
-      location, // URL remains unchanged
+      location,
       opening_time,
       closing_time,
       days_open: days_open?.toUpperCase(),
@@ -48,13 +50,30 @@ const createTouristSpotController = async (req, res) => {
       rules: rules?.toUpperCase()
     });
 
-    res.json(touristSpot);
+    // Upload images to S3
+    if (req.files && req.files.length > 0) {
+      const imageUrls = [];
+      for (const file of req.files.slice(0, 5)) {
+        const s3Key = `tourist_spots/${Date.now()}_${file.originalname}`;
+        const uploadParams = {
+          Bucket: process.env.AWS_S3_BUCKET,
+          Key: s3Key,
+          Body: file.buffer,
+          ContentType: file.mimetype,
+        };
+        await s3Client.send(new PutObjectCommand(uploadParams));
+        const imageUrl = `https://${process.env.AWS_S3_BUCKET}.s3.${process.env.AWS_REGION}.amazonaws.com/${s3Key}`;
+        imageUrls.push(imageUrl);
+      }
+      await uploadTouristSpotImages(spot.id, imageUrls);
+    }
+
+    res.json(spot);
   } catch (err) {
     console.log(err.message);
-    res.send(err.message);
+    res.status(500).send(err.message);
   }
 };
-
 
 const editTouristSpotController = async (req, res) => {
   try {
@@ -75,10 +94,10 @@ const editTouristSpotController = async (req, res) => {
       contact_number,
       email,
       facebook_page,
-      rules
+      rules,
     } = req.body;
 
-    const touristSpot = await editTouristSpot(touristSpotId, {
+    const spot = await editTouristSpot(touristSpotId, {
       name: name?.toUpperCase(),
       type: type?.toUpperCase(),
       description: description?.toUpperCase(),
@@ -97,43 +116,46 @@ const editTouristSpotController = async (req, res) => {
       rules: rules?.toUpperCase()
     });
 
-    res.json(touristSpot);
+    res.json(spot);
   } catch (err) {
     console.log(err.message);
-    res.send(err.message);
+    res.status(500).send(err.message);
   }
 };
-
 
 const deleteTouristSpotController = async (req, res) => {
   try {
     const { touristSpotId } = req.params;
-    const touristSpot = await deleteTouristSpot(touristSpotId);
-    res.json(touristSpot);
+
+    // Delete associated images
+    await deleteTouristSpotImages(touristSpotId);
+
+    const result = await deleteTouristSpot(touristSpotId);
+    res.json(result);
   } catch (err) {
     console.log(err.message);
-    res.send(err.message);
+    res.status(500).send(err.message);
   }
 };
 
 const viewTouristSpotsController = async (req, res) => {
   try {
-    const touristSpots = await getAllTouristSpots();
-    res.json(touristSpots);
+    const spots = await getAllTouristSpots();
+    res.json(spots);
   } catch (err) {
     console.log(err.message);
-    res.send(err.message);
+    res.status(500).send(err.message);
   }
 };
 
 const viewTouristSpotByIdController = async (req, res) => {
   try {
     const { touristSpotId } = req.params;
-    const touristSpot = await getTouristSpotById(touristSpotId);
-    res.json(touristSpot);
+    const spot = await getTouristSpotById(touristSpotId);
+    res.json(spot);
   } catch (err) {
     console.log(err.message);
-    res.send(err.message);
+    res.status(500).send(err.message);
   }
 };
 
