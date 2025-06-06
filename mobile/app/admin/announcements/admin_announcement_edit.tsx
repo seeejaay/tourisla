f5047234b31b9091556105fc2d73cf718c6e2aac
ztreuse <tjzbay13@gmail.com>
2025-06-06 21:36:30 +0800

    edit

--- mobile/app/admin/announcements/admin_announcement_edit.tsx
+++ mobile/app/admin/announcements/admin_announcement_edit.tsx
@@ -523,6 +523,7 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.1,
     shadowRadius: 4,
     elevation: 3,
+    marginTop: 50 + STATUS_BAR_HEIGHT,
   },
   formGroup: {
     marginBottom: 16,
