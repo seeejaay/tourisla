d8bf4f855094acbf43886984e9f29af59c43b571
Carl <carljustinem984@gmail.com>
2025-06-26 01:22:56 +0800

    update

--- web/next.config.ts
+++ web/next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
   typescript: { ignoreBuildErrors: true },
   allowedDevOrigins: [
     "http://localhost:3000",
