f77e7e960df6f5b667a10532150b882595e3a577
Carl <carljustinem984@gmail.com>
2025-06-27 18:32:47 +0800

    update debug

--- web/src/app/static/islandEntry/islandEntryFields.ts
+++ web/src/app/static/islandEntry/islandEntryFields.ts
@@ -2,10 +2,30 @@ import { GroupMember } from "@/app/islandEntry-regis/page";
 
 export const islandEntryFields = [
   { name: "name", label: "Name", type: "text" },
-  { name: "sex", label: "Sex", type: "select", options: ["Male", "Female", "Other"] },
+  {
+    name: "sex",
+    label: "Sex",
+    type: "select",
+    options: ["Male", "Female", "Other"],
+  },
   { name: "age", label: "Age", type: "number" },
   { name: "is_foreign", label: "Foreign?", type: "checkbox" },
-  { name: "municipality", label: "Municipality", type: "text", showIf: (v: GroupMember) => !v.is_foreign },
-  { name: "province", label: "Province", type: "text", showIf: (v: GroupMember) => !v.is_foreign },
-  { name: "country", label: "Country", type: "text", showIf: (v: GroupMember) => v.is_foreign },
+  {
+    name: "municipality",
+    label: "Municipality",
+    type: "text",
+    showIf: (v: GroupMember) => !v.is_foreign,
+  },
+  {
+    name: "province",
+    label: "Province",
+    type: "text",
+    showIf: (v: GroupMember) => !v.is_foreign,
+  },
+  {
+    name: "country",
+    label: "Country",
+    type: "text",
+    showIf: (v: GroupMember) => v.is_foreign,
+  },
 ];
