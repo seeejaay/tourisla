0714c790d87440d0a7b133538a3131c0c1e15dd4
Carl <carljustinem984@gmail.com>
2025-06-28 04:33:18 +0800

    update

--- backend/db/index.js
+++ backend/db/index.js
@@ -10,7 +10,10 @@ const pool = new Pool({
   database: process.env.PGDATABASE,
   password: process.env.PGPASSWORD,
   port: process.env.PGPORT,
-  ssl: { rejectUnauthorized: false },
+  ssl: {
+    require: true,
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = {
