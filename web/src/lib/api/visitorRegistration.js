70e3ffa9004dc4b25282943783951d494e3ab50a
Carl <carljustinem984@gmail.com>
2025-06-27 14:51:46 +0800

    update

--- web/src/lib/api/visitorRegistration.js
+++ web/src/lib/api/visitorRegistration.js
@@ -119,6 +119,7 @@ export const getVisitorResult = async (uniqueCode) => {
 
 export const getQRCodebyUserId = async (userId) => {
   try {
+    console.log("Fetching QR Code for User ID:", userId);
     const response = await axios.get(`${API_URL}register/qr`, {
       withCredentials: true,
     });
