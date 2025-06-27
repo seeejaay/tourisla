6292c1471a50ea0986212e3204e54ec6eec1d2ed
Carl <carljustinem984@gmail.com>
2025-06-28 04:28:08 +0800

    update

--- backend/db/index.js
+++ backend/db/index.js
@@ -5,11 +5,11 @@ const { Pool } = require("pg");
 const isProd = process.env.NODE_ENV === "production";
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
+  port: process.env.PGPORT,
   ssl: { rejectUnauthorized: false },
 });
 
