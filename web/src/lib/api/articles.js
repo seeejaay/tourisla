4e8d79843e1d7efc2b77237f5fe5127ea1a3b155
Carl <carljustinem984@gmail.com>
2025-06-29 13:41:51 +0800

    update

--- web/src/lib/api/articles.js
+++ web/src/lib/api/articles.js
@@ -21,9 +21,19 @@ export const createArticle = async (data) => {
 };
 
 export const updateArticle = async (articleId, data) => {
-  const res = await axios.put(`${API_URL}articles/${articleId}`, data, {
+  let config = {
     withCredentials: true,
-  });
+  };
+
+  // If data is FormData (for file upload), let browser set headers
+  if (typeof FormData !== "undefined" && data instanceof FormData) {
+    // Do not set Content-Type, axios will handle it
+  } else {
+    // For JSON, set Content-Type
+    config.headers = { "Content-Type": "application/json" };
+  }
+
+  const res = await axios.put(`${API_URL}articles/${articleId}`, data, config);
   return res.data;
 };
 
