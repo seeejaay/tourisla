5d76f59433c4c2ea6d79cd58ef07f07164e46485
Carl <carljustinem984@gmail.com>
2025-07-09 19:56:43 +0800

    update

--- backend/utils/webhook.js
+++ backend/utils/webhook.js
@@ -8,7 +8,7 @@ const createPayMongoWebhook = async () => {
       {
         data: {
           attributes: {
-            url: "https://tourisla-production.up.railway.app/api/v1/paymongo/webhook", // railway public URL
+            url: "https://tourisla-production-5c54.up.railway.app/api/v1/paymongo/webhook", // railway public URL
             events: ["link.payment.paid"],
           },
         },
@@ -19,15 +19,20 @@ const createPayMongoWebhook = async () => {
           "content-type": "application/json",
           authorization:
             "Basic " +
-            Buffer.from(process.env.PAYMONGO_SECRET_KEY + ":").toString("base64"),
+            Buffer.from(process.env.PAYMONGO_SECRET_KEY + ":").toString(
+              "base64"
+            ),
         },
       }
     );
 
     console.log("Webhook created: ", response.data.data);
-    return response.data.data; 
+    return response.data.data;
   } catch (error) {
-    console.error("Failed to create webhook: ", error.response?.data || error.message);
+    console.error(
+      "Failed to create webhook: ",
+      error.response?.data || error.message
+    );
     throw error;
   }
 };
