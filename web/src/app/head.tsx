4fc3b8d262e5c7b99c71d4290531e22a14adfdf8
Carl <carljustinem984@gmail.com>
2025-08-01 06:17:37 +0800

    update

---
+++ web/src/app/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <link rel="icon" href="/favicon.ico" sizes="any" />
+      <link rel="icon" type="image/png" href="/TourISLA_logo.png" />
+      {/* Optional: Apple Touch Icon */}
+      <link rel="apple-touch-icon" href="/logo.png" />
+    </>
+  );
+}
